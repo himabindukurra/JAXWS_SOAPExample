16b1fdf5dcb551cbfebe14798ba235f251a34d4c
Himabindu Kurra <kurrahimabindhu@gmail.com>
2019-01-03 20:03:40 -0500

    first commit

---
+++ src/com/webservice/soap/PersonService.java
@@ -0,0 +1,15 @@
+package com.webservice.soap;
+
+
+import com.webservice.soap.Person;
+
+public interface PersonService {
+
+	public boolean addPerson(Person p);
+	
+	public boolean deletePerson(int id);
+	
+	public Person getPerson(int id);
+	
+	public Person[] getAllPersons();
+}
